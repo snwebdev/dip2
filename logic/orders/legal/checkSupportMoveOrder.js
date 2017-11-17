a8331669949b7573893d5b9f923eab753cbb1cce
Stewart <stewart@snwebdev.com>
2017-11-17 13:59:05 +0000

    seems to work at this stage, but parser needs tightening

--- logic/orders/legal/checkSupportMoveOrder.js
+++ logic/orders/legal/checkSupportMoveOrder.js
@@ -2,18 +2,18 @@ const orderPowerOwnsUnitInProvince = require('./orderPowerOwnsUnitInProvince');
 const supportedMoveFromProvinceIsOccupied = require('./supportedMoveFromProvinceIsOccupied');
 const supportedUnitOrderedToMove = require('./supportedUnitOrderedToMove');
 const supportingUnitCouldMovToSupportedProvince = require('./moveSupportingUnitCouldMoveToSupportedProvince');
-
+const couldSupportMoveTo = require('../../map/couldSupportMoveTo');
 module.exports = function (units, orders, order) {
     order.orderPowerOwnsUnitInProvince = orderPowerOwnsUnitInProvince(units, order);
     order.supportedMoveFromProvinceIsOccupied = supportedMoveFromProvinceIsOccupied(units, order);
     order.supportedUnitOrderedToMove = supportedUnitOrderedToMove(units, orders, order);
-    order.supportingUnitCouldMoveToSupportedUnitProvince = supportingUnitCouldMovToSupportedProvince(units, order);
+    order.supportingUnitCanSupportMoveToSupportedMoveToProvince = couldSupportMoveTo(order.unitType, order.unitLocation,order.supportMoveToName );
 
     if (
         order.orderPowerOwnsUnitInProvince &&
         order.supportedMoveFromProvinceIsOccupied &&
         order.supportedUnitOrderedToMove &&
-        order.supportingUnitCouldMoveToSupportedUnitProvince
+        order.supportingUnitCanSupportMoveToSupportedMoveToProvince
     ) {
         return true;
     } else {
